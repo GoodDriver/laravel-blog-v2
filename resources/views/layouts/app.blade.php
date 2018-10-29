c0cbc4628ef5d022a6d5ec49634cc3f262a4cf14
ZHAOGJ <zhaogj@alltosun.com>
2018-10-29 19:23:49 +0800

    ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ public_path('favicon.ico') }}" />
-    <link rel="bookmark" href="{{ public_path('favicon.ico') }}" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="bookmark" href="favicon.ico" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
